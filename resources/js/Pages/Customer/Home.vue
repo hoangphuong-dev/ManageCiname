a44195f60e0d283907b41b7b15564336953bd4c2
phuonght <phuonght@rabiloo.com>
2022-02-14 16:05:23 +0700

    do page customer

--- resources/js/Pages/Customer/Home.vue
+++ resources/js/Pages/Customer/Home.vue
@@ -2,10 +2,19 @@
     <app-layout title="Dashboard">
         <div class="py-12">
             <div class="mx-auto messenger-window__custom">
-
                 <div class="w-full">
                     <div class="main-jobs">
-                        Đây là main job
+                        <!-- slider -->
+                        <div class="w-full">
+                            <div class="w-4/5 m-auto border-white h-44" >
+                                <vue-slider v-model="value" />
+                            </div>
+                        </div>
+                        <div class="w-full">
+                            <div class="w-1/2">
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -13,50 +22,52 @@
     </app-layout>
 </template>
 
-// <script>
+<script>
 // import Filter from "../../../Components/Job/Filter.vue";
 // import Notification from "../../../Components/Job/Notification.vue";
 // import JobItem from "@/Components/Job/JobItem.vue";
 // import Pagination from "@/Components/Pagination.vue";
-
-// export default {
-//     components: { Filter, Notification, JobItem, Pagination },
-//     props: {
-//         tags: Array,
-//         hospital_types: Array,
-//     },
-//     created() {
-//         this.fetchData();
-//     },
-//     data: function () {
-//         return {
-//             jobs: [],
-//             loading: false,
-//             total: "",
-//             perPage: "",
-//             filter: {
-//                 page: 1,
-//                 limit: 10,
-//             },
-//         };
-//     },
-//     methods: {
-//         handleCurrentPage(value) {
-//             this.filter.page = value;
-//             this.fetchData();
-//         },
-//         async fetchData() {
-//             this.loading = true;
-//             await this.axios
-//                 .get(`jobs`, { params: this.filter })
-//                 .then(async (res) => {
-//                     this.jobs = res.data;
-//                     this.total = res.data.meta.total;
-//                     this.perPage = res.data.meta.per_page;
-//                 })
-//                 .catch(() => {});
-//             this.loading = false;
-//         },
-//     },
-// };
-// </script>
+import VueSlider from 'vue-slider-component'
+import 'vue-slider-component/theme/antd.css'
+export default {
+    components: {VueSlider },
+    props: {
+        tags: Array,
+        hospital_types: Array,
+    },
+    created() {
+        // this.fetchData();
+    },
+    data: function () {
+        return {
+            value: 0,
+            slider: [1,2,3,4,5,5],
+            loading: false,
+            total: "",
+            perPage: "",
+            filter: {
+                page: 1,
+                limit: 10,
+            },
+        };
+    },
+    methods: {
+        handleCurrentPage(value) {
+            this.filter.page = value;
+            this.fetchData();
+        },
+        async fetchData() {
+            this.loading = true;
+            await this.axios
+                .get(`jobs`, { params: this.filter })
+                .then(async (res) => {
+                    this.jobs = res.data;
+                    this.total = res.data.meta.total;
+                    this.perPage = res.data.meta.per_page;
+                })
+                .catch(() => {});
+            this.loading = false;
+        },
+    },
+};
+</script>
