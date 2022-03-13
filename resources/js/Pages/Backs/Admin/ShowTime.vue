259d6c862c5f83c81ef9d63a06b29fdb4c3c90af
Phuonght <hoangphuong2311bkhn@gmail.com>
2022-03-14 00:01:48 +0700

    init full calender

--- resources/js/Pages/Backs/Admin/ShowTime.vue
+++ resources/js/Pages/Backs/Admin/ShowTime.vue
@@ -1,13 +1,69 @@
 <template>
-  <div class="p-4"></div>
+  <div class="p-4">
+    <FullCalendar :events="events" :options="options" />
+  </div>
 </template>
 
 <script>
 import AdminLayout from "@/Layouts/Admin/AdminLayout.vue";
+import "@fullcalendar/core/vdom"; // solves problem with Vite
+import FullCalendar from "@fullcalendar/vue3";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
 export default {
   name: "ShowTime",
   components: {
     AdminLayout,
+    FullCalendar,
+  },
+  data() {
+    return {
+      options: {
+        plugins: [dayGridPlugin, timeGridPlugin],
+        initialDate: "2019-01-01",
+        headerToolbar: {
+          left: "prev,next",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek",
+        },
+        editable: true,
+      },
+      events: [
+        {
+          id: 1,
+          title: "All Day Event",
+          start: "2019-01-01",
+        },
+        {
+          id: 2,
+          title: "Long Event",
+          start: "2019-01-07",
+          end: "2019-01-10",
+        },
+        {
+          id: 3,
+          title: "Repeating Event",
+          start: "2019-01-09T16:00:00",
+        },
+        {
+          id: 4,
+          title: "Repeating Event",
+          start: "2019-01-16T16:00:00",
+        },
+        {
+          id: 5,
+          title: "Conference",
+          start: "2019-01-11",
+          end: "2019-01-13",
+        },
+        {
+          id: 6,
+          title: "Meeting",
+          start: "2019-01-12T10:30:00",
+          end: "2019-01-12T12:30:00",
+        },
+      ],
+    };
   },
 };
 </script>
