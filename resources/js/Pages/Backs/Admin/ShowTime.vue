3a3536a021c1ddcc1481c91973ebcea2da511c58
Phuonght <hoangphuong2311bkhn@gmail.com>
2022-03-17 00:39:53 +0700

    fix UI calender

--- resources/js/Pages/Backs/Admin/ShowTime.vue
+++ resources/js/Pages/Backs/Admin/ShowTime.vue
@@ -1,96 +1,193 @@
-<template>
-  <div class="p-4">
-    <FullCalendar :events="events" :options="options" />
-  </div>
-</template>
-
 <script>
-import AdminLayout from "@/Layouts/Admin/AdminLayout.vue";
-import "@fullcalendar/core/vdom"; // solves problem with Vite
 import FullCalendar from "@fullcalendar/vue3";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import resourceTimeGridPlugin from "@fullcalendar/resource-timegrid";
+import { INITIAL_EVENTS, createEventId } from "./event-utils";
+
 export default {
-  name: "ShowTime",
   components: {
-    AdminLayout,
-    FullCalendar,
+    FullCalendar, // make the <FullCalendar> tag available
   },
-  data() {
+
+  data: function () {
     return {
-      options: {
-        plugins: [dayGridPlugin, timeGridPlugin],
-        initialDate: "2019-01-01",
+      calendarOptions: {
+        plugins: [
+          dayGridPlugin,
+          timeGridPlugin,
+          interactionPlugin,
+          resourceTimeGridPlugin, // needed for dateClick
+        ],
         headerToolbar: {
-          left: "prev,next",
+          left: "prev,next today",
           center: "title",
-          right: "dayGridMonth,timeGridWeek",
+          right: "dayGridMonth,timeGridWeek,timeGridDay",
         },
-        titleFormat: {
-          year: "numeric",
-          month: "2-digit",
-          day: "numeric",
-          weekday: "long",
-        },
-        buttonText: {
-          today: "Hôm nay",
-          month: "Tháng",
-          week: "Tuần ",
-          day: "Ngày",
-          list: "list",
-        },
-        timeZone: "Asia/Ho_Chi_Minh",
+        initialView: "timeGridDay", // swithch this to resourceTimeGridDay to see resource bug
+        initialEvents: INITIAL_EVENTS, // alternatively, use the `events` setting to fetch from a feed
         editable: true,
+        selectable: true,
+        selectMirror: true,
+        dayMaxEvents: true,
+        weekends: true,
+        select: this.handleDateSelect,
+        eventClick: this.handleEventClick,
+        eventsSet: this.handleEvents,
+        resources: this.myResources,
+        events: this.myEvents,
+        /* you can update a remote database when these fire:
+        eventAdd:
+        eventChange:
+        eventRemove:
+        */
       },
-      events: [
+      currentEvents: [],
+      myResources: [{ id: 1, title: "works" }],
+      myEvents: [
         {
           id: 1,
-          title: "All Day Event",
-          start: "2019-01-01",
-        },
-        {
-          id: 2,
-          title: "Long Event",
-          start: "2019-01-07",
-          end: "2019-01-10",
+          title: "All-day event",
+          start: new Date().toISOString().replace(/T.*$/, ""),
         },
         {
           id: 3,
-          title: "Repeating Event",
-          start: "2019-01-09T16:00:00",
-        },
-        {
-          id: 4,
-          title: "Repeating Event",
-          start: "2019-01-16T16:00:00",
-        },
-        {
-          id: 5,
-          title: "Conference",
-          start: "2019-01-11",
-          end: "2019-01-13",
-        },
-        {
-          id: 6,
-          title: "Meeting",
-          start: "2019-01-12T10:30:00",
-          end: "2019-01-12T12:30:00",
+          title: "Timed event",
+          start: new Date().toISOString().replace(/T.*$/, "") + "T12:00:00",
         },
       ],
     };
   },
+
+  methods: {
+    handleWeekendsToggle() {
+      this.calendarOptions.weekends = !this.calendarOptions.weekends; // update a property
+    },
+
+    handleDateSelect(selectInfo) {
+      let title = prompt("Please enter a new title for your event");
+      let calendarApi = selectInfo.view.calendar;
+
+      calendarApi.unselect(); // clear date selection
+
+      if (title) {
+        calendarApi.addEvent({
+          id: createEventId(),
+          title,
+          start: selectInfo.startStr,
+          end: selectInfo.endStr,
+          allDay: selectInfo.allDay,
+        });
+      }
+    },
+
+    handleEventClick(clickInfo) {
+      if (
+        confirm(
+          `Are you sure you want to delete the event '${clickInfo.event.title}'`
+        )
+      ) {
+        clickInfo.event.remove();
+      }
+    },
+
+    handleEvents(events) {
+      this.currentEvents = events;
+    },
+  },
 };
 </script>
 
-<style>
-.fc .fc-button-primary {
-  background: #ff7777;
-  margin-right: 4px;
-  border-color: #ff7777;
+<template>
+  <div class="demo-app">
+    <div class="demo-app-sidebar">
+      <div class="demo-app-sidebar-section">
+        <h2>Instructions</h2>
+        <ul>
+          <li>Select dates and you will be prompted to create a new event</li>
+          <li>Drag, drop, and resize events</li>
+          <li>Click an event to delete it</li>
+        </ul>
+      </div>
+      <div class="demo-app-sidebar-section">
+        <label>
+          <input
+            type="checkbox"
+            :checked="calendarOptions.weekends"
+            @change="handleWeekendsToggle"
+          />
+          toggle weekends
+        </label>
+      </div>
+      <div class="demo-app-sidebar-section">
+        <h2>All Events ({{ currentEvents.length }})</h2>
+        <ul>
+          <li v-for="event in currentEvents" :key="event.id">
+            <b>{{ event.startStr }}</b>
+            <i>{{ event.title }}</i>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="demo-app-main">
+      <FullCalendar class="demo-app-calendar" :options="calendarOptions">
+        <template v-slot:eventContent="arg">
+          <b>{{ arg.timeText }}</b>
+          <i>{{ arg.event.title }}</i>
+        </template>
+      </FullCalendar>
+    </div>
+  </div>
+</template>
+
+<style lang='css'>
+h2 {
+  margin: 0;
+  font-size: 16px;
 }
-.fc .fc-button-primary:not(:disabled).fc-button-active {
-  background: #ff7777;
-  border-color: #ff7777;
-  margin-right: 4px;
+
+ul {
+  margin: 0;
+  padding: 0 0 0 1.5em;
+}
+
+li {
+  margin: 1.5em 0;
+  padding: 0;
+}
+
+b {
+  /* used for event dates/times */
+  margin-right: 3px;
+}
+
+.demo-app {
+  display: flex;
+  min-height: 100%;
+  font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+  font-size: 14px;
+}
+
+.demo-app-sidebar {
+  width: 300px;
+  line-height: 1.5;
+  background: #eaf9ff;
+  border-right: 1px solid #d3e2e8;
+}
+
+.demo-app-sidebar-section {
+  padding: 2em;
+}
+
+.demo-app-main {
+  flex-grow: 1;
+  padding: 3em;
+}
+
+.fc {
+  /* the calendar root */
+  max-width: 1100px;
+  margin: 0 auto;
 }
 </style>
