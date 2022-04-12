6074e77d710d1cd2ef4b3d22f23113f23dbe78a5
Hoang Trong Phuong <phuonght@rabiloo.com>
2022-04-12 16:39:57 +0700

    fix deploy

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,11 +16,11 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" href="{{ '/css/app.css' }}">
 
     <!-- Scripts -->
     @routes
-    <script src="{{ mix('js/app.js') }}"></script>
+    <script src="{{ '/js/app.js' }}"></script>
 
 </head>
 
