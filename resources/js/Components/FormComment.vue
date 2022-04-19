8979cfc6e7db8c1f7223e19da4beae9d58e0e87b
Hoang Trong Phuong <phuonght@rabiloo.com>
2022-04-20 01:44:23 +0700

    add reply comment

--- resources/js/Components/FormComment.vue
+++ resources/js/Components/FormComment.vue
@@ -1,44 +1,131 @@
 <template>
   <div>
-    <h3 class="text-center py-6">Bình luận về phim</h3>
+    <h3 class="text-center pb-6">Bình luận về phim</h3>
     <!-- list comment  -->
     <div v-for="item in comments" :key="item.id">
+      <div v-if="item.parent_id === 0">
+        <div class="flex">
+          <el-avatar
+            shape="square"
+            :size="50"
+            :src="getImage(item?.user.image) || '/uploads/customer.png'"
+          />
+          <h4 class="mt-4 ml-4">{{ item?.user.name }}</h4>
+        </div>
+        <div class="w-full my-2 border p-2 rounded">{{ item.content }}</div>
+        <div class="w-full mb-8">
+          <span class="cursor-pointer mx-2 font-bold hover:text-blue-500">
+            Thích
+          </span>
+          <span
+            @click="reply(item.id)"
+            class="cursor-pointer mx-2 font-bold hover:text-blue-500"
+            >Trả lời
+          </span>
+          <span class="cursor-pointer mx-2 font-bold hover:text-blue-500">
+            {{ item.created_at }}
+          </span>
+        </div>
+      </div>
+
+      <!-- show comment children  -->
+      <div v-for="item in comments" :key="item.id">
+        <div v-if="item.parent_id === item.id">
+          <div class="flex">
+            <el-avatar
+              shape="square"
+              :size="50"
+              :src="getImage(item?.user.image) || '/uploads/customer.png'"
+            />
+            <h4 class="mt-4 ml-4">{{ item?.user.name }}</h4>
+          </div>
+          <div class="w-full my-2 border p-2 rounded">{{ item.content }}</div>
+          <div class="w-full mb-8">
+            <span class="cursor-pointer mx-2 font-bold hover:text-blue-500">
+              Thích
+            </span>
+            <span
+              @click="reply(item.id)"
+              class="cursor-pointer mx-2 font-bold hover:text-blue-500"
+              >Trả lời
+            </span>
+            <span class="cursor-pointer mx-2 font-bold hover:text-blue-500">
+              {{ item.created_at }}
+            </span>
+          </div>
+        </div>
+      </div>
+      <!-- end show comment children -->
+
+      <!-- rep comment  -->
+      <div
+        v-if="showFormReply === item.id"
+        style="width: calc(100% - 2.5rem)"
+        class="mb-6 ml-10"
+      >
+        <div>
+          <div class="flex">
+            <el-avatar
+              shape="square"
+              :size="50"
+              :src="getImage(user?.image) || '/uploads/customer.png'"
+            />
+            <h4 class="mt-4 ml-4">{{ user?.name }}</h4>
+          </div>
+          <div class="w-full mt-2">
+            <el-form ref="form" :model="form" :rules="rules">
+              <el-form-item prop="content">
+                <el-input
+                  v-model="form.content"
+                  :rows="5"
+                  type="textarea"
+                  placeholder="Viết bình luận ..."
+                  @keyup.ctrl.enter="submit()"
+                />
+              </el-form-item>
+            </el-form>
+          </div>
+
+          <div class="text-center mt-4">
+            <el-button @click="submit()" type="success" plain
+              >Bình luận</el-button
+            >
+          </div>
+        </div>
+      </div>
+      <!-- end rep comment  -->
+    </div>
+    <!-- end list comment  -->
+
+    <!-- form comment  -->
+    <!-- <div>
       <div class="flex">
         <el-avatar
           shape="square"
           :size="50"
-          :src="getImage(item?.user.image) || '/uploads/customer.png'"
+          :src="getImage(user?.image) || '/uploads/customer.png'"
         />
-        <h4 class="mt-4 ml-4">{{ item?.user.name }}</h4>
+        <h4 class="mt-4 ml-4">{{ user?.name }}</h4>
       </div>
-      <div class="w-full my-2 border p-2 rounded">{{ item.content }}</div>
-      <div class="w-full mb-10">Thích / Trả lời /{{ item.created_at }}</div>
-    </div>
+      <div class="w-full mt-2">
+        <el-form ref="form" :model="form" :rules="rules">
+          <el-form-item prop="content">
+            <el-input
+              v-model="form.content"
+              :rows="5"
+              type="textarea"
+              placeholder="Viết bình luận ..."
+              @keyup.ctrl.enter="submit()"
+            />
+          </el-form-item>
+        </el-form>
+      </div>
 
-    <div class="flex">
-      <el-avatar
-        shape="square"
-        :size="50"
-        :src="getImage(user?.image) || '/uploads/customer.png'"
-      />
-      <h4 class="mt-4 ml-4">{{ user?.name }}</h4>
-    </div>
-    <div class="w-full mt-2">
-      <el-form ref="form" :model="form" :rules="rules">
-        <el-form-item prop="content">
-          <el-input
-            v-model="form.content"
-            :rows="5"
-            type="textarea"
-            placeholder="Viết bình luận ..."
-            @keyup.ctrl.enter="submit()"
-          />
-        </el-form-item>
-      </el-form>
-    </div>
-    <div class="text-center mt-4">
-      <el-button @click="submit()" type="success" plain>Bình luận</el-button>
-    </div>
+      <div class="text-center mt-4">
+        <el-button @click="submit()" type="success" plain>Bình luận</el-button>
+      </div>
+    </div> -->
+    <!-- end form comment  -->
   </div>
 </template>
 
@@ -60,6 +147,7 @@ export default {
 
   data: function () {
     return {
+      showFormReply: "",
       comments: [],
       form: {
         movie_id: "",
@@ -72,7 +160,6 @@ export default {
           {
             required: true,
             message: "Nội dung bình luận không được để trống !",
-            trigger: "change",
           },
         ],
       },
@@ -84,6 +171,12 @@ export default {
   },
 
   methods: {
+    reply(id) {
+      this.showFormReply = id;
+      this.form.parent_id = id;
+      console.log(88888, id);
+    },
+
     async fetchData() {
       console.log(99999);
       getCommentMovie()
@@ -107,6 +200,7 @@ export default {
           this.form.content = "";
         }
       });
+      this.showFormReply = "";
       this.fetchData();
     },
 
