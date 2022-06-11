bb7f138aa7079ebce23a0c655925599a86c4cc74
Hoang Trong Phuong <phuonght@rabiloo.com>
2022-06-11 23:06:02 +0700

    implement shouldqueue

--- app/Notifications/RegisterStaff.php
+++ app/Notifications/RegisterStaff.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class RegisterStaff extends Notification
+class RegisterStaff extends Notification implements ShouldQueue
 {
     use Queueable;
 
